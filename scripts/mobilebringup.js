b3ca1f00e0ae84c7bcfffad334e1c5a8f5ee602f
KadynCBR <kadyn_martinez@hotmail.com>
2016-07-03 20:02:04 -0700

    Project pages now responsive.

--- scripts/mobilebringup.js
+++ scripts/mobilebringup.js
@@ -1,11 +1,24 @@
+// $(document).on("pageinit", "#body-container", function(){
+    // $("#body-container").on("swiperight",function() {
+    //   $("#cbp-spmenu-s1").addClass("cbp-spmenu-open");
+    // });
+    // $("#body-container").on("swipeleft", function() {
+    //   $("#cbp-spmenu-s1").removeClass("cbp-spmenu-open");
+    // });
+// });
+// $(document).on('swipeleft', '#body-container', function(e) {
+  // console.log("Swipeleft");
+  // });
 $(function() {
-  $("#body-container").on("swiperight",function() {
-    $("#cbp-spmenu-s1").addClass("cbp-spmenu-open");
-  });
-  $("#body-container").on("swipeleft", function() {
-    $("#cbp-spmenu-s1").removeClass("cbp-spmenu-open");
-  });
-  $("#swipe-indicator").mousedown(function() {
+  $("#swipe-indicator").mousedown(function(e) {
     $("#cbp-spmenu-s1").toggleClass("cbp-spmenu-open");
+    e.stopPropagation();
   });
+  $("#body-container").mousedown(function() {
+      if( $("#cbp-spmenu-s1").hasClass("cbp-spmenu-open") ) {
+        $("#cbp-spmenu-s1").toggleClass("cbp-spmenu-open");
+      }
+  });
+
+
 });
